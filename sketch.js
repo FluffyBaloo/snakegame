e01fb5e2069ba100621ac298e564223adf9074d0
FluffyBaloo <32744572+FluffyBaloo@users.noreply.github.com>
2020-04-15 18:26:04 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,57 +1,55 @@
 let snake;
-let rez = 20;
+const rez = 20;
 let food;
 let w;
 let h;
-
+let dirs;
 
 function setup() {
-  createCanvas(400, 400);
-  w = floor(width / rez);
-  h = floor(height/ rez);
-  frameRate(5);
-  snake = new Snake();
-  foodLocation();
+	createCanvas(400, 400);
+	w = floor(width / rez);
+	h = floor(height / rez);
+	frameRate(5);
+	snake = new Snake();
+	
+	dirs = {
+		[LEFT_ARROW]: createVector(-1, 0),
+		[RIGHT_ARROW]: createVector(1, 0),
+		[DOWN_ARROW]: createVector(0, 1),
+		[UP_ARROW]: createVector(0, -1)
+	};
+	
+	foodLocation();
 }
 
 function foodLocation() {
-  let x = floor(random(w));
-  let y = floor(random(h));
-  food = createVector(x, y);
+	let x = floor(random(w));
+	let y = floor(random(h));
+	food = createVector(x, y);
 }
 
-
 function keyPressed() {
-
-  if (keyCode === LEFT_ARROW) {
-    snake.setDir(-1, 0);
-  } else if (keyCode === RIGHT_ARROW) {
-    snake.setDir(1, 0);
-  } else if (keyCode === DOWN_ARROW) {
-    snake.setDir(0, 1);
-  } else if (keyCode === UP_ARROW) {
-    snake.setDir(0, -1);
-  } else if (key == '  ') {
-    snake.grow();
-  }
+	var dir = dirs[keyCode];
+	if (dir) {
+		snake.setDir(dir);
+	}
 }
 
 function draw() {
-  scale(rex);
-  background(220);
-  if (snake.eat(food)) {
-    foodLocation();
-  }
-  snake.update();
-  snake.show();
+	scale(rez);
+	background(220);
+	if (snake.update(food)) {
+		foodLocation();
+	}
+	snake.show();
 
-  if (snake.endGame()) {
-    print("END GAME");
-    background(255, 0, 0);
-    noLoop();
-  }
+	if (snake.endGame()) {
+		print("END GAME");
+		background(255, 0, 0);
+		noLoop();
+	}
 
-  noStroke();
-  fill(255, 0, 0);
-  rect(food.x, food.y, 1, 1);
+	noStroke();
+	fill(100, 0, 0);
+	rect(food.x, food.y, 1, 1);
 }
