dd151ae072127d1dc1495f8f76bce00b531e3f5e
FluffyBaloo <32744572+FluffyBaloo@users.noreply.github.com>
2022-11-10 21:19:27 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+let snake;
+const rez = 20;
+let food;
+let w;
+let h;
+let dirs;
+let bImg;
+var song;
+
+function preload() {
+  bImg = loadImage('background.jpg');
+  song = loadSound("hyper.mp3");
+  
+  
+} 
+
+function setup() {
+	createCanvas(400, 400);
+	w = floor(width / rez);
+	h = floor(height / rez);
+	frameRate(5);
+    song.loop();
+    song.setVolume(0.5);
+	snake = new Snake();
+	
+	dirs = {
+		[LEFT_ARROW]: createVector(-1, 0),
+		[RIGHT_ARROW]: createVector(1, 0),
+		[DOWN_ARROW]: createVector(0, 1),
+		[UP_ARROW]: createVector(0, -1)
+	};
+	
+	foodLocation();
+}
+
+function foodLocation() {
+	let x = floor(random(w));
+	let y = floor(random(h));
+	food = createVector(x, y);
+}
+
+function keyPressed() {
+	var dir = dirs[keyCode];
+	if (dir) {
+		snake.setDir(dir);
+	}
+}
+
+function draw() {
+  
+	scale(rez);
+	background(bImg);
+	if (snake.update(food)) {
+		foodLocation();
+	}
+	snake.show();
+
+	if (snake.endGame()) {
+		print("END GAME");
+		background(255, 0, 0);
+		noLoop();
+        song.stop();
+	}
+
+	noStroke();
+	fill(255, 50, 255);
+	rect(food.x, food.y, 1, 1);
+}
---
+++ snake.js
@@ -0,0 +1,48 @@
+class Snake {
+
+	constructor() {
+		this.body = [];
+		this.head = createVector(floor(w / 2), floor(h / 2));
+		this.dir = createVector(0, 0);
+	}
+
+	setDir(dir) {
+		this.dir = dir;
+	}
+
+	update(food) {
+		this.body.push(this.head.copy());
+		this.head.add(this.dir);
+		if (this.head.x === food.x && this.head.y === food.y) {
+			return true; // yum!
+		}
+		this.body.shift();
+		return false;
+	}
+
+	endGame() {
+		let x = this.head.x;
+		let y = this.head.y;
+		if (x > w - 1 || x < 0 || y > h - 1 || y < 0) {
+			return true;
+		}
+		for (let part of this.body) {
+			if (part.x == x && part.y == y) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+	show() {
+		fill(0);
+		noStroke();
+		for (let part of this.body) {
+			rect(part.x, part.y, 1, 1);
+		}
+		fill(0, 255,255);
+		rect(this.head.x, this.head.y, 1, 1);
+	}
+
+}
